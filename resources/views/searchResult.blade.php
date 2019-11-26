246b75b85c72735aec9884dfea9d31352155aa05
Blueoxmtl <g.brunet96@gmail.com>
2019-11-26 17:47:25 -0500

    max-height img and link to product in search

--- resources/views/searchResult.blade.php
+++ resources/views/searchResult.blade.php
@@ -1,4 +1,4 @@
 
     @foreach($products as $product)
-    <div><img style="max-width:50%" src = "{{ asset('storage/' . $product->image_path) }}"/> {{$product->name}}</div>
+        <a href="{{route("customer.product", ['id'=>$product->id])}}"> <div><img style="max-width:50%" src = "{{ asset('storage/' . $product->image_path) }}"/> {{$product->name}}</div></a>
     @endforeach
